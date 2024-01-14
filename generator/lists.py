37dabe370c4b2633157f1695e0e360abaeec24df
Kyoto67 <whynot.2@icloud.com>
2024-01-14 21:10:13 +0300

    add: generator

---
+++ generator/lists.py
@@ -0,0 +1,203 @@
+trial_names = [
+    "Hunter Exam",
+    "Zoldyck Family is Testing Gate",
+    "Heavens Arena Matches",
+    "Yorknew City Auction",
+    "Greed Island Game",
+    "Chimera Ant Extermination",
+    "Heir is War Succession War",
+    "Dark Continent Expedition",
+]
+
+trial_descriptions = [
+    "A rigorous and challenging examination that tests the skills, intelligence, and adaptability of aspiring Hunters. Participants face various trials, including survival tests, combat challenges, and ethical dilemmas, designed to select only the most exceptional individuals.",
+    "An exclusive test imposed by the formidable Zoldyck family to assess the capabilities and resolve of those seeking their services. The testing gate is a series of life-threatening obstacles, each designed to weed out the weak and ensure only the strongest can approach the Zoldyck estate.",
+    "A multi-tiered fighting tournament held in the towering Heavens Arena. Combatants from around the world participate in one-on-one battles, climbing through the ranks to prove their martial prowess. The arena provides a stage for Nen users to showcase their abilities.",
+    "A grand event where rare and valuable items are auctioned off in the bustling city of Yorknew. However, beneath the surface of commerce lies a complex web of intrigue and danger, as various factions vie for control and conflicts unfold during the auction.",
+    "An immersive and mysterious video game created by the enigmatic game designer Ging Freecss. Players enter a virtual world filled with challenges, treasures, and creatures. The game becomes a quest for those seeking elusive rewards and the secrets of the real world connection to Greed Island.",
+    "A perilous mission to eradicate the Chimera Ants, a species that poses a significant threat to humanity due to its ability to evolve rapidly. Hunters and nations join forces to combat the Chimera Ants and their King, facing moral dilemmas and powerful adversaries.",
+    "A deadly competition among the Kakin Royal Family is princes to determine the next ruler. The Succession War involves a brutal battle where princes use Nen beasts to eliminate their rivals, and only one can emerge as the sole heir to the throne.",
+    "A highly secretive and perilous expedition to the unexplored and mysterious Dark Continent. Undertaken by elite Hunters, the journey involves navigating treacherous territories, encountering unknown creatures, and unveiling the ancient mysteries hidden within the forbidden land.",
+]
+
+trial_rules = [
+    "Participants must adhere to a strict code of conduct, demonstrating respect and integrity throughout the entire examination process.",
+    "No external assistance is allowed during the Zoldyck Family is Testing Gate. Participants must rely solely on their own abilities to overcome the challenges.",
+    "In Heavens Arena Matches, combatants must follow the arena is rules and regulations, including no use of lethal force and respecting the decisions of the referee.",
+    "During the Yorknew City Auction, participants are prohibited from engaging in any form of violence within the auction premises. Violators will be immediately disqualified and removed.",
+    "Greed Island Game participants must play by the rules of the game, solving challenges and puzzles to progress. Cheating or exploiting game mechanics is strictly forbidden.",
+    "In the Chimera Ant Extermination mission, hunters must prioritize the safety of civilians and fellow hunters. Reckless behavior that endangers others will result in severe consequences.",
+    "The Heir is War Succession War imposes a rule of absolute secrecy. Participants must conceal their involvement and strategies, engaging in the battle discreetly.",
+    "Dark Continent Expedition members must strictly follow the expedition leader is commands. Disobeying orders jeopardizes the safety of the entire team.",
+]
+
+trial_group_descriptions = [
+    "Survival Trials: Participants are thrust into harsh environments where their ability to endure, adapt, and strategize in challenging conditions is tested.",
+    "Combat Challenges: Focused on testing participants martial skills, combat challenges involve one-on-one or group battles against formidable opponents.",
+    "Intellectual Tests: Trials designed to assess participants intelligence, problem-solving abilities, and strategic thinking in complex scenarios.",
+    "Morality Dilemmas: Participants face ethical and moral challenges, forcing them to make difficult decisions that reveal their character and values.",
+    "Team Cooperation Trials: Emphasizing teamwork, participants must collaborate effectively to overcome obstacles and complete tasks.",
+    "Stealth and Infiltration: Participants are tasked with navigating through secure areas undetected, testing their stealth, agility, and resourcefulness.",
+    "Psychological Examinations: Trials that delve into the mental resilience and psychological fortitude of participants through illusions, mind games, and illusions.",
+    "Survivor is Quest: A series of interconnected trials where participants progress through a storyline, facing evolving challenges and plot twists.",
+]
+
+team_names = [
+    "Phantom Troupe",
+    "Shadow Beasts",
+    "Royal Guards",
+    "Chimera Ant Squadron Leaders",
+    "Zodiacs",
+    "Beyond Netero is Expedition Team",
+    "Heaven is Arena Fighters",
+    "Greedy Ghosts",
+    "Silent Majority",
+    "Kakin Expedition Security Team",
+    "Moritonio Brothers",
+    "Cobra Royal Army",
+    "Seaquant is Squad",
+    "Nostrade Family Bodyguards",
+    "Extermination Team",
+]
+
+guilds_and_factions = [
+    "Zoldyck Family",
+    "Hunters",
+    "Phantom Troupe",
+    "Black Whale",
+    "Spiders",
+    "Shadow Beasts",
+    "Blue Planet",
+    "Master Hunter",
+    "White Shooter",
+    "Spider Leader",
+    "Silver Eye",
+    "The Magician",
+    "Wanderer",
+    "Great Sage",
+    "Enlightened One",
+    "Crusher",
+    "Master of the Game",
+    "Ryodan 1",
+    "Ryodan 2",
+    "Ryodan 3",
+    "Ryodan 4",
+    "Ryodan 5",
+    "Ryodan 6",
+    "Ryodan 7",
+    "Ryodan 8",
+    "Ryodan 9",
+    "Ryodan 10",
+    "Ryodan 11",
+    "Ryodan 12",
+    "Ryodan 13"
+]
+
+
+names = [
+    "Gon",
+    "Killua",
+    "Kurapika",
+    "Leorio",
+    "Hisoka",
+    "Chrollo",
+    "Illumi",
+    "Kite",
+    "Biscuit",
+    "Ging",
+    "Mito",
+    "Knuckle",
+    "Shoot",
+    "Palm",
+    "Meruem",
+    "Komugi",
+    "Neferpitou",
+    "Shaiapouf",
+    "Feitan",
+    "Nobunaga",
+]
+
+surnames = [
+    "Freecss",
+    "Zoldyck",
+    "Zoldyck",
+    "Paradinight",
+    "Lucilfer",
+    "Zoldyck",
+    "Krueger",
+    "Freecss",
+    "Freecss",
+    "Bine",
+    "McMahon",
+    "Siberia",
+    "Portor",
+    "Hazama",
+]
+
+interactions = [
+    "Two individuals form a bond during a challenging exam.",
+    "A character seeks revenge for a personal tragedy.",
+    "An altruistic act of self-sacrifice to save another.",
+    "A person breaks away from a restrictive background to forge their own path.",
+    "The protagonist embarks on a journey to uncover hidden truths.",
+    "A mysterious character engages in unpredictable interactions with others.",
+    "Various alliances are formed to confront a common threat.",
+    "Loyalty and emotional connections among powerful entities.",
+    "A mentor figure leaves a lasting impact on the development of young hunters.",
+    "A group of individuals with unique abilities navigate complex dynamics.",
+    "Political maneuvering and power struggles within an organization.",
+    "Rivalry and conflicting philosophies between influential figures.",
+    "Epic battles between characters with extraordinary abilities.",
+    "Negotiations and alliances in a high-stakes political environment.",
+]
+
+character_groups = [
+    "Hunters Association Members",
+    "Zoldyck Family Members",
+    "Phantom Troupe Members",
+    "Chimera Ant Royal Guards",
+    "Kakin Royal Family Princes",
+    "Heaven is Arena Fighters",
+    "Greed Island Game Players",
+    "Extermination Team Members",
+    "Dark Continent Expedition Participants",
+    "Greedy Ghosts",
+    "Nostrade Family Members",
+    "Zodiacs - Hunter Association",
+    "Beyond Netero is Expedition Team",
+    "Kakin Royal Army Members",
+    "Shadow Beasts",
+    "Seaquant is Squad",
+]
+
+hunter_guild_positions = [
+    "Guild Master",
+    "Vice Guild Master",
+    "Senior Hunter",
+    "Examiner",
+    "Trainer",
+    "Mission Coordinator",
+    "Field Researcher",
+    "Combat Specialist",
+    "Intelligence Analyst",
+    "Medical Officer",
+    "Infiltration Expert",
+    "Beast Hunter",
+    "Archivist",
+    "Navigational Specialist",
+    "Artifact Collector"
+]
+
+statuses_arr = [
+    'ДОПУЩЕН К ИСПЫТАНИЮ',
+    'В ПРОЦЕССЕ ИСПЫТАНИЯ',
+    'ПРОШЕЛ ИСПЫТАНИЕ',
+    'ВЫБЫЛ В ПРОЦЕССЕ ИСПЫТАНИЯ',
+    'ДИСКВАЛИФИЦИРОВАН В ПРОЦЕССЕ ИСПЫПЫТАНИЯ',
+    'УМЕР В ПРОЦЕССЕ ИСПЫТАНИЯ',
+    'ДОПУЩЕН К ПОЛУЧЕНИЮ ЗВАНИЯ'
+]
+
+weights = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+]
---
+++ generator/main.py
@@ -0,0 +1,136 @@
+import datetime
+import string
+import random
+from datetime import datetime, timedelta
+from lists import *
+
+hunters_fullnames_in_db = []
+candidates_fullnames_in_db = []
+trials_in_db = []
+
+def sql_str_arrs_converter(arr):
+    result = '\'{'
+    for item in arr:
+        result+='\"'+item+'\", '
+    result = result[:-2]
+    result+='}\''
+    return result
+
+def sql_int_arrs_converter(arr):
+    result = '\'{'
+    for item in arr:
+        result+=str(item)+', '
+    result = result[:-2]
+    result+='}\''
+    return result
+
+def sql_str_converter(str):
+    return '\'' + str + '\''
+
+def generate_random_string(length=10):
+    characters = string.ascii_letters + string.digits  # буквы и цифры
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    return random_string
+
+def generate_random_timestamp():
+    start_date = datetime(2100, 1, 1)
+    random_timedelta = timedelta(days=random.randint(0, 365 * 20), hours=random.randint(0, 24), minutes=random.randint(0,60), seconds=random.randint(0,60))
+    random_timestamp = start_date + random_timedelta
+    formatted_timestamp = random_timestamp.strftime("%Y/%m/%d %H:%M:%S")
+    return formatted_timestamp
+
+def generate_random_integer(min_value=1, max_value=100):
+    return random.randint(min_value, max_value)
+
+def generate_random_array(array, len=1):
+    return random.sample(array, len)
+
+def hunters():
+    first_name = random.choice(names)
+    last_name = random.choice(surnames)
+    hunters_fullnames_in_db.append(first_name + ' ' + last_name)
+    post = random.choice(hunter_guild_positions)
+    return f"INSERT INTO hunters_guild (first_name, last_name, post) VALUES ({sql_str_converter(first_name)},{sql_str_converter(last_name)}, {sql_str_converter(post)});\n"
+
+def statuses():
+    return f"insert into status (description) values ('ДОПУЩЕН К ИСПЫТАНИЮ'), ('В ПРОЦЕССЕ ИСПЫТАНИЯ'), ('ПРОШЕЛ ИСПЫТАНИЕ'), ('ВЫБЫЛ В ПРОЦЕССЕ ИСПЫТАНИЯ'), ('ДИСКВАЛИФИЦИРОВАН В ПРОЦЕССЕ ИСПЫПЫТАНИЯ'), ('УМЕР В ПРОЦЕССЕ ИСПЫТАНИЯ'),  ('ДОПУЩЕН К ПОЛУЧЕНИЮ ЗВАНИЯ');"
+
+def candidates():
+    first_name = random.choice(names)
+    last_name = random.choice(surnames)
+    candidates_fullnames_in_db.append(first_name + ' ' + last_name)
+    arrs_len = generate_random_integer(1,10)
+    facts_arr = generate_random_array(guilds_and_factions, arrs_len)
+    weights_arr = generate_random_array(weights, arrs_len)
+    return f"call add_candidate_and_info({sql_str_converter(first_name)}, {sql_str_converter(last_name)}, {sql_str_arrs_converter(facts_arr)}, {sql_int_arrs_converter(weights_arr)});"
+
+def trials():
+    organizators_arr = generate_random_array(hunters_fullnames_in_db, generate_random_integer(1,3))
+    rules_arr = generate_random_array(trial_rules, generate_random_integer(1,3))
+    trial = random.choice(trial_names)
+    trials_in_db.append(trial)
+    trial_decs = random.choice(trial_descriptions)
+    return f"call add_trial_and_rules({sql_str_arrs_converter(organizators_arr)}, {sql_str_arrs_converter(rules_arr)}, {sql_str_converter(trial)}, {sql_str_converter(trial_decs)});"
+
+def teams():
+    team = random.choice(team_names)
+    team_desc = 'BAD BOYS'
+    members_arr = generate_random_array(candidates_fullnames_in_db, generate_random_integer(1, 10))
+    return f"select add_members_or_create_team({sql_str_converter(team)}, {sql_str_converter(team_desc)}, {sql_str_arrs_converter(members_arr)});"
+
+def interaction():
+    members_arr = generate_random_array(candidates_fullnames_in_db, generate_random_integer(1,10))
+    interaction_group = random.choice(character_groups)
+    interaction_desc = random.choice(interactions)
+    t_start = generate_random_timestamp()
+    t_end = generate_random_timestamp()
+    while (t_end<=t_start):
+        t_start = generate_random_timestamp()
+        t_end = generate_random_timestamp()
+    return f"call add_interaction_group({sql_str_arrs_converter(members_arr)}, {sql_str_converter(interaction_group)}, {sql_str_converter(interaction_desc)}, {sql_str_converter(t_start)}, {sql_str_converter(t_end)});"
+
+def tournament():
+    trials_titles = generate_random_array(trials_in_db,3)
+    trials_group = random.choice(trial_group_descriptions)
+    return f"select create_tournament({sql_str_arrs_converter(trials_titles)}, {sql_str_converter(trials_group)});"
+
+def trial_in_process():
+    return f"INSERT INTO trial_in_process (tournament_id, trial_id, candidate_id) VALUES (1, {generate_random_integer(1,30)}, {generate_random_integer(1,70)});"
+
+def trials_hist():
+    return f"INSERT INTO trials_history (candidate_id, trial_id, trial_status, tournament_id) VALUES ({generate_random_integer(1,70)}, {generate_random_integer(1,30)}, (select id from status where description = {sql_str_converter(random.choice(statuses_arr))}), 1);"
+
+FILENAME = "generated_dml.sql"
+with open(FILENAME, 'w') as file:
+    file.write(statuses() + '\n')
+    file.write("\n")
+
+    for i in range(70):
+        file.write(candidates() + '\n')
+    file.write("\n")
+
+    for i in range(70):
+        file.write(hunters())
+    file.write("\n")
+
+    for i in range(30):
+        file.write(trials() + '\n')
+    file.write("\n")
+
+    for i in range(30):
+        file.write(teams() + '\n')
+    file.write("\n")
+
+    for i in range(50):
+        file.write(interaction() + '\n')
+    file.write("\n")
+
+    file.write(tournament() + '\n')
+    file.write("\n")
+    for i in range(20):
+        file.write(trial_in_process() + '\n')
+    file.write("\n")
+
+    for i in range(20):
+        file.write(trials_hist() + '\n')
+    file.write("\n")
