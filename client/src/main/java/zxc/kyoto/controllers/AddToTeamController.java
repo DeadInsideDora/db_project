80a2da12b34fced008f3cc096f0a99772829f4b3
Kyoto67 <whynot.2@icloud.com>
2024-01-24 09:39:29 +0300

    intermediate commit

---
+++ client/src/main/java/zxc/kyoto/controllers/AddToTeamController.java
@@ -0,0 +1,27 @@
+package zxc.kyoto.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import zxc.kyoto.entity.Request;
+import zxc.kyoto.util.ClientService;
+import zxc.kyoto.util.UserContainer;
+
+public class AddToTeamController {
+    @FXML
+    private TextField CandidateLastName;
+    @FXML
+    private TextField candidateFirstName;
+    @FXML
+    private TextField teamTitle;
+    @FXML
+    private Label addMemberError;
+
+
+    @FXML
+    private void addMemberToTeam(){
+        Request request = new Request(UserContainer.getUser(), new Object[]{"add_team", teamTitle.getText(), new String[]{candidateFirstName.getText() + " " + CandidateLastName.getText()}});
+        String response = ClientService.service(request);
+        addMemberError.setText(response);
+    }
+}
