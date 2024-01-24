bf2c22b88b91a111ed96df06cb9a89210e7b47e9
Kyoto67 <whynot.2@icloud.com>
2024-01-24 10:54:56 +0300

    testing

--- client/src/main/java/zxc/kyoto/controllers/CreateTrialController.java
+++ client/src/main/java/zxc/kyoto/controllers/CreateTrialController.java
@@ -15,13 +15,15 @@ public class CreateTrialController {
     @FXML
     private TextField rules;
     @FXML
+    private TextField trialDescription;
+    @FXML
     private Label error;
 
     @FXML
     public void createTrial() {
         String[] orgs = organizators.getText().split(", ");
         String[] rls = rules.getText().split(", ");
-        Request request = new Request(UserContainer.getUser(), new Object[]{"add_trial", newTrial.getText(), "zxc", orgs, rls});
+        Request request = new Request(UserContainer.getUser(), new Object[]{"add_trial", newTrial.getText(), trialDescription.getText(), orgs, rls});
         String response = ClientService.service(request);
         error.setText(response);
     }
